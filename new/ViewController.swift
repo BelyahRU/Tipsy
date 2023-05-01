d4bf9db1dd2f5b619bdf523abcd20d0ec5600d37
BelyahRU <oBelyash@yandex.ru>
2023-05-01 07:45:57 +0300

    Initial Commit

---
+++ new/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Александр Андреев on 01.05.2023.
+//
+
+import SnapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialize()
+    }
+    
+    private func initialize() {
+        let background = UIColor(red: 241/255, green: 238/255, blue: 228/255, alpha: 1)
+        let label = UILabel()
+        label.text = "✌️ Welcome!"
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(50)
+            make.top.equalToSuperview().inset(150)
+        }
+    }
+}
+
